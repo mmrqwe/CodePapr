73b0dc46acbd52ef9e09fdd8705ee6d0e281f1f9
mmrqwe <10875484+mmrqwe@users.noreply.github.com>
2026-07-11 16:03:34 +0800

    update

--- .cargo-vendor/css/bindings/python/tree_sitter_css/__init__.pyi
+++
@@ -1,6 +0,0 @@
-from typing import Final
-from typing_extensions import CapsuleType
-
-HIGHLIGHTS_QUERY: Final[str]
-
-def language() -> CapsuleType: ...
--- .cargo-vendor/kotlin/tools/vitest.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vitest/config';
-export default defineConfig({
-  test: {
-    include: ['tools/**/*.test.js'],
-  },
-});
